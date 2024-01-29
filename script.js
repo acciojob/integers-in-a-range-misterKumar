01bc2dc3bbb80af6743dce866a934510af552b7e
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-01-29 21:04:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 // write your code here
+function range(start, end) {
+  if (start > end) {
+    return [];
+  } else if (start === end) {
+    return [start];
+  }
 
-// do not change the code below.
+  const result = [];
+  for (let i = start; i <= end; i++) {
+    result.push(i);
+  }
+
+  return result;
+}
+
 let start = parseInt(prompt("Enter Start of the Range."));
 let end = parseInt(prompt("Enter End Of the Range."));
 alert(range(start, end));
