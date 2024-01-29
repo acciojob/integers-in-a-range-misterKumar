f1fd9ad81fe62ea8ce846659b426ad39ed3fb2aa
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-01-29 21:07:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 // write your code here
-function range(start, end) {
-  if (start > end) {
-    return [];
-  } else if (start === end) {
-    return [start];
-  }
+function range(start, end){
 
-  const result = [];
-  for (let i = start; i <= end; i++) {
-    result.push(i);
-  }
-
-  return result;
+	if(start <= end)
+	{
+		let result = [];  
+		for(let i=start+1; i<end; i++)
+			{
+				result.push(i);
+           } 
+		return result;
+	}
+	else
+	{
+		return "Enter the Valid Numbers.";
+	} 
 }
+
+// do not change the code below.
 
 let start = parseInt(prompt("Enter Start of the Range."));
 let end = parseInt(prompt("Enter End Of the Range."));
